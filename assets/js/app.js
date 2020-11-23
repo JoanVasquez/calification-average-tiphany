f2db01b7787cfd371ee1ca204da826317f303818
Joan.Vasquez <joan.vasquez@atl-software.net>
2020-11-23 16:13:25 -0400

    working final steps

---
+++ assets/js/app.js
@@ -0,0 +1,96 @@
+(function () {
+  const califications = [];
+
+  const onPostCalification = () => {
+    document
+      .getElementById("calification=form")
+      .addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const subject = document.getElementById("subject").value;
+        const calification = document.getElementById("calification").value;
+
+        const calificationValidated = validations.calificationValidation(
+          calification
+        );
+
+        if (validations.subjectValidation(subject)) {
+          alert("Inserte una materia");
+          return;
+        } else if (validations.calificationValidation(calification)) {
+          alert(calificationValidated);
+          return;
+        }
+
+        addCalification({ subject, calification });
+      });
+  };
+
+  const validations = {
+    subjectValidation: (subject) => subject.length === 0,
+    calificationValidation: (calification) => {
+      if (!calification.length) {
+        return "Inserte una nota";
+      } else if (isNaN(calification)) {
+        return "La nota no es un numero";
+      } else if (calification < 0 || calification > 5) {
+        return "La nota debe ser entre 1 - 5";
+      }
+    },
+  };
+
+  const addCalification = ({ subject, calification }) => {
+    if (califications.length) {
+      const isRepeated = califications.find((calItem) => {
+        if (calItem) {
+          return calItem.subject === subject;
+        }
+      });
+      if (isRepeated) {
+        alert(`${subject} ya existe!`);
+        return;
+      }
+    }
+
+    califications.push({
+      subject: subject,
+      calification: calification,
+    });
+
+    const calificationBody = document.getElementById("calification-body");
+    calificationBody.innerHTML = generateTable();
+  };
+
+  window.onRemoveCalification = (index) => {
+    califications[index] = null;
+    const calificationBody = document.getElementById("calification-body");
+    calificationBody.innerHTML = generateTable();
+  };
+
+  const generateTable = () => {
+    let result = "";
+    let promedio = 0;
+    let total = 0;
+    califications.forEach((calification, index) => {
+      if (calification) {
+        total += 1;
+        promedio += Number(calification.calification);
+        result += `<tr>
+                            <td>${calification.subject}</td>
+                            <td>${calification.calification}</td>
+                            <td class="text-center" onClick="onRemoveCalification(${index})">
+                                <i class="far fa-trash-alt text-danger"></i>
+                            </td>
+                        </tr>`;
+      }
+    });
+
+    result += `<tr>
+                  <td class="bg-dark text-white">Promedio</td>
+                  <td>${promedio / total}</td>
+                 </tr>`;
+    return result;
+  };
+
+  onPostCalification();
+})();
